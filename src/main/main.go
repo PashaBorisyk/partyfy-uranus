b1363a3cb268f35b29fe58d4c90ca2c0b2429895
pborissiouk <pborissiouk@bluesoft.net.pl>
2018-11-29 20:54:28 +0100

    library added

--- src/main/main.go
+++ src/main/main.go
@@ -1,36 +1,51 @@
 package main
 
 import (
-	"golang.org/x/net/websocket"
 	"log"
-	"net/http"
+	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "0.0.0.0"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
 
-	http.Handle("/", websocket.Handler(serverClientConnection))
-	err := http.ListenAndServe(":8081", nil)
+	log.Print("Starting program...")
+
+	listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+
 	if err != nil {
 		log.Print("Error while serving connections")
 	}
 
-}
-
-func serverClientConnection(ws *websocket.Conn) {
-
-	addr := ws.RemoteAddr().String()
-	log.Print(addr)
-
-	bytesToRead := make([]byte, 1024)
-	log.Print(ws.Request().Header)
-
-	log.Print("Headers are : ")
-	for k, v := range ws.Request().Header {
-		log.Print(k, " |======|  ", v)
-	}
+	defer listener.Close()
+	log.Print("Listening on ", CONN_HOST, " : ", CONN_PORT)
 
 	for {
-		read, err := ws.Read(bytesToRead)
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Print("Error accepting : ", err.Error())
+			os.Exit(1)
+		}
+
+		go serverClientConnection(connection)
+
+	}
+
+}
+
+func serverClientConnection(conn net.Conn) {
+
+	log.Print("Connection from ", conn.RemoteAddr())
+
+	buf := make([]byte, 1024)
+
+	for {
+		read, err := conn.Read(buf)
 		if err != nil {
 			log.Print("Error")
 			log.Print(err)
